0526a963dd96f7493c4e4bb9b3141eeac3bca369
Jeremy Gobert <jeremy.gobert@student.ehb.be>
2021-02-02 15:33:55 +0100

    erty

--- backend/node.js
+++ backend/node.js
@@ -7,11 +7,14 @@ const {
     request,
     response
 } = require('express');
-const dialogflow = require('@google-cloud/dialogflow').v2beta1;
+const {
+    WebhookClient
+} = require('dialogflow-fulfillment');
 const uuid = require('uuid');
 const app = express();
 const util = require('util');
 const requesting = require('request');
+const axios = require('axios');
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({
@@ -53,57 +56,53 @@ app.post('/save', (req, res) => {
 });
 
 
-app.post('/dialogflow', (req, res) => {
-    async function runSample(projectId = 'travelbot-pxus') {
-        // A unique identifier for the given session
-        const sessionId = uuid.v4();
+app.post('/dialogflow', express.json(), (req, res) => {
+    const agent = new WebhookClient({
+        request: req,
+        response: res
+    });
 
-        // Create a new session
-        const sessionClient = new dialogflow.SessionsClient();
-        const sessionPath = sessionClient.projectAgentSessionPath(
-            projectId,
-            sessionId
-        );
-
-        // The text query request.
-        const request = {
-            session: sessionPath,
-            queryInput: {
-                text: {
-                    // The query to send to the dialogflow agent
-                    text: 'hello',
-                    // The language used by the client (en-US)
-                    languageCode: 'en-US',
-                },
-            },
-        };
-
-        // Send request and log result
-        const responses = await sessionClient.detectIntent(request);
-        console.log('Detected intent');
-        const result = responses[0].queryResult;
-        res.send(`  Response: ${result.fulfillmentText}`);
-        console.log(`  Query: ${result.queryText}`);
-        console.log(`  Response: ${result.fulfillmentText}`);
-        if (result.intent) {
-            console.log(`  Intent: ${result.intent.displayName}`);
-        } else {
-            console.log('  No intent matched.');
-        }
-    }
-    const args = process.argv.slice(2);
-    if (args.length !== 1) {
-        console.error(`
-    USAGE:
-        node quickstart.js <projectId>
-    EXAMPLE:
-        node quickstart.js my-project-id
-    You can find your project ID in your Dialogflow agent settings:  https://dialogflow.com/docs/agents#settings.
-    `);
-        process.exit(1);
+    function welcome() {
+        agent.add('Welcome to my agent!');
     }
 
-    runSample(...args).catch(console.error);
+    async function places(agent) {
+        let location = agent.parameters["geo-city"];
+        let lel = agent.parameters["geo-city"];
+        var agentString = "";
+        let lon = 0;
+        let lat = 0;
+
+        axios.get(`https://api.opentripmap.com/0.1/en/places/geoname?name=${location}&apikey=5ae2e3f221c38a28845f05b62d1ac562a2f1db0199e756e903a2a654`)
+            .catch(function (error) {
+                console.log(error);
+                console.log('fuck');
+            }).then(function (response) {
+                let body = response.data;
+                lon = body.lon;
+                lat = body.lat;
+                axios.get(`https://api.opentripmap.com/0.1/en/places/radius?radius=5000&lon=${lon}&lat=${lat}&limit=10&apikey=5ae2e3f221c38a28845f05b62d1ac562a2f1db0199e756e903a2a654`)
+                    .catch(function (error) {
+                        console.log(error);
+                        console.log('fuck');
+                    }).then(function (response) {
+                        let body = response.data;
+                        agentString = "Here is a list of some interesting places to vistit in " + location + ": ";
+                        body.features.forEach(place => {
+                            agentString += place.properties.name + ", \n";
+                        });
+                        console.log(agentString);
+                        agent.add(agentString);
+                    });
+            });
+    }
+
+
+    let intentMap = new Map();
+    intentMap.set('Default Welcome Intent', welcome);
+    intentMap.set('Default Place Intent', places);
+
+    agent.handleRequest(intentMap);
 });
 
 
@@ -112,7 +111,7 @@ app.post('/translate', (req, res) => {
     async function translation() {
         let origin = req.query.translate;
         let from = req.query.from;
-        let to = req.query.to;  
+        let to = req.query.to;
 
         const options2 = {
             method: 'POST',
@@ -130,13 +129,37 @@ app.post('/translate', (req, res) => {
                 target: to
             }
         };
+        var options = {
+            method: 'POST',
+            url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded',
+                'accept-encoding': 'application/gzip',
+                'x-rapidapi-key': '2bc4ce673fmshc52b545e3b0cb23p114533jsn02827fd189e0',
+                'x-rapidapi-host': 'google-translate1.p.rapidapi.com'
+            },
+            data: {
+                q: 'Hello, world!',
+                source: 'en',
+                target: 'es'
+            }
+        };
 
-        requesting(options2, function (error, response, body) {
+        axios.request(options).then(function (response) {
+            console.log(response.data);
+            let trans = JSON.parse(response.data);
+            console.log(trans.data.translations[0].translatedText);
+            res.send(trans.data.translations[0].translatedText);
+        }).catch(function (error) {
+            console.error(error);
+        });
+
+        /* requesting(options2, function (error, response, body) {
             if (error) throw new Error(error);
             let trans = JSON.parse(body);
             console.log(trans.data.translations[0].translatedText);
             res.send(trans.data.translations[0].translatedText);
-        });
+        }); */
 
     }
     translation();
@@ -158,4 +181,3 @@ function setHtmlData() {
     let link = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=belgium&inputtype=textquery&key=AIzaSyAyiPIrfJd1nzyZYu4myv4w-5ubdkxzjU0`;
     loadAllData(link);
 }
-
